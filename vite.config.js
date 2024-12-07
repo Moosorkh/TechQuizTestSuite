a24ea18c5b75ff002f47b45b0d3f3f58c59f55ca
fortis <moosorkh@gmail.com>
2024-12-06 19:20:41 -0800

    initial commit

--- vite.config.js
+++ vite.config.js
@@ -8,17 +8,5 @@ export default defineConfig({
     globals: true,
     environment: 'jsdom',
     setupFiles: './src/_tests_/setup.ts'
-  },
-  server: {
-    port: 3001,
-    host: '127.0.0.1',
-    open: false,
-    proxy: {
-      '/api': {
-        target: 'http://localhost:3001',
-        secure: false,
-        changeOrigin: true
-      }
-    }
   }
 })
